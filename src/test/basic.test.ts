6a26365864d6c9b1ed56155eb914258a8551921f
ShrJamal <me@shrjamal.com>
2022-11-03 11:05:42 +0100

    add vitest

---
+++ src/test/basic.test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from 'vitest'
+test('Basic Test', () => {
+  const res = 1 + 4
+
+  expect(res).to.eq(5)
+})
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './src',
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
