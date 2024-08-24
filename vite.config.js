152d6c99060741ebe5c77c156a4e145992c3cd79
Jamal Shr <shrjamal.dev@gmail.com>
2024-08-24 23:06:13 +0100

    feat: add tailwindcss v4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   root: './src',
+  publicDir: '../public',
   server: {
     port: 3000,
   },
+  plugins: [tsconfigPaths(), tailwindcss()],
 })
