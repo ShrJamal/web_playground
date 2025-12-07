9ec9a40a54ddcfd9782d4e203b0ed8720db49acf
Jamal Shr <shrjamal.dev@gmail.com>
2025-12-07 17:10:03 +0100

    feat: integrate WebSocket for real-time updates and add zod dependency

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,20 @@ import viteLogo from "/vite.svg"
 export default function App() {
   const [count, setCount] = createSignal(0)
 
+  const ws = new WebSocket("/api/ws")
+  ws.addEventListener("open", () => {
+    console.log("WebSocket connected")
+  })
+  ws.addEventListener("message", (ev) => {
+    console.log("WebSocket message received", ev.data)
+    setCount(Number(ev.data))
+  })
+  ws.addEventListener("close", () => {
+    console.log("WebSocket closed")
+  })
+  ws.addEventListener("error", (event) => {
+    console.error("WebSocket error", event)
+  })
   return (
     <div class="flex-1 flex flex-col gap-4 items-center justify-center">
       <img
@@ -16,6 +30,7 @@ export default function App() {
         class="btn btn-primary"
         onClick={() => {
           setCount((p) => p + 1)
+          ws.send(count().toString())
         }}
         type="button"
       >
