c2a06355c589cef44b8a622268546769297a29b7
Jamal Shr <me@shrjamal.com>
2026-06-13 13:22:23 +0100

    migrate to vite-plus and add linting and formatting configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vite-plus"
 
 export default defineConfig({
   root: "./src",
@@ -8,5 +7,55 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsconfigPaths(), tailwindcss()],
+  plugins: [tailwindcss()],
+  lint: {
+    options: { typeAware: true, typeCheck: true },
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    plugins: ["import", "node", "vitest"],
+    env: {
+      builtin: true,
+    },
+    rules: {
+      "vite-plus/prefer-vite-plus-imports": "error",
+      "no-unused-vars": [
+        "warn",
+        // {
+        //   fix: {
+        //     imports: "safe-fix",
+        //   },
+        // },
+      ],
+    },
+    ignorePatterns: [
+      "**/*.d.ts",
+      "**/*.gen.ts",
+      "**/public/**",
+      "**/*.js",
+      "bun.lock",
+    ],
+  },
+  fmt: {
+    useTabs: false,
+    tabWidth: 2,
+    printWidth: 80,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    quoteProps: "as-needed",
+    trailingComma: "all",
+    semi: false,
+    arrowParens: "always",
+    bracketSameLine: false,
+    bracketSpacing: true,
+    singleAttributePerLine: true,
+    ignorePatterns: [
+      "**/*.d.ts",
+      "**/*.gen.ts",
+      "**/public/**",
+      "**/*.js",
+      "bun.lock",
+    ],
+  },
+  staged: {
+    "*.{ts,tsx,html,css}": "vp check --fix",
+  },
 })
