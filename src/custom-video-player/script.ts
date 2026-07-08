27e487dc95c34f9f16e1654f90bfa49f50d6c1cf
Jamal Shr <shrjamal.dev@gmail.com>
2026-07-08 11:21:33 +0100

    migrate to vite-plus

--- src/custom-video-player/script.ts
+++ src/custom-video-player/script.ts
@@ -1,38 +1,38 @@
-import './style.css'
+import "./style.css"
 
 let isPlaying = false
 let progress = 0
-let timestamp = '00:00'
+let timestamp = "00:00"
 
-const videoRef = document.querySelector<HTMLVideoElement>('#video')
-videoRef?.addEventListener('click', togglerPlayer)
-videoRef?.addEventListener('ended', stopPlayer)
+const videoRef = document.querySelector<HTMLVideoElement>("#video")
+videoRef?.addEventListener("click", togglerPlayer)
+videoRef?.addEventListener("ended", stopPlayer)
 // videoRef?.addEventListener('timeupdate', seek)
 
-const playBtn = document.querySelector('#play')
-playBtn?.addEventListener('click', togglerPlayer)
+const playBtn = document.querySelector("#play")
+playBtn?.addEventListener("click", togglerPlayer)
 
-const stopBtn = document.querySelector('#stop')
-stopBtn?.addEventListener('click', stopPlayer)
+const stopBtn = document.querySelector("#stop")
+stopBtn?.addEventListener("click", stopPlayer)
 
-const progressRef = document.querySelector<HTMLInputElement>('#progress')
-progressRef?.addEventListener('change', seek)
+const progressRef = document.querySelector<HTMLInputElement>("#progress")
+progressRef?.addEventListener("change", seek)
 
-const timestampRef = document.querySelector<HTMLHeadElement>('#timestamp')
+const timestampRef = document.querySelector<HTMLHeadElement>("#timestamp")
 
 setInterval(() => {
   if (!progressRef || !timestampRef) return
-  progressRef.value = progress + ''
+  progressRef.value = progress + ""
   timestampRef.innerText = timestamp
 }, 1000)
 
-videoRef?.addEventListener('timeupdate', () => {
+videoRef?.addEventListener("timeupdate", () => {
   progress = (videoRef.currentTime / videoRef.duration) * 100
   let minutes = Math.floor(videoRef.currentTime / 60).toString()
-  minutes = (minutes.length < 2 ? '0' : '') + minutes
+  minutes = (minutes.length < 2 ? "0" : "") + minutes
 
   let seconds = Math.floor(videoRef.currentTime % 60).toString()
-  seconds = (seconds.length < 2 ? '0' : '') + seconds
+  seconds = (seconds.length < 2 ? "0" : "") + seconds
   timestamp = `${minutes}:${seconds}`
   if (videoRef.ended) {
     isPlaying = false
@@ -43,10 +43,10 @@ function togglerPlayer() {
   isPlaying = !isPlaying
   if (isPlaying) {
     videoRef?.play()
-    playBtn?.classList.remove('fa-play')
+    playBtn?.classList.remove("fa-play")
   } else {
     videoRef?.pause()
-    playBtn?.classList.add('fa-play')
+    playBtn?.classList.add("fa-play")
   }
 }
 function stopPlayer() {
